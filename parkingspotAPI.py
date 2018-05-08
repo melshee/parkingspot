f2e35eaed18f84150fd44c9643759ecbbe589e4e
Melissa Sheehan <mxsheehan7@gmail.com>
2018-05-07 22:56:54 -0700

    reserve request handles normal input and checks for whether a in can be reserved

--- parkingspotAPI.py
+++ parkingspotAPI.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from pCoordinates import coordinates
+import sys
 
 app = Flask(__name__)
 
@@ -10,7 +11,7 @@ def hello_world():
 
 @app.route('/available', methods=['GET', 'POST'])
 def getAvailableSpots():
-    if request.method == 'POST': #this block is only entered when the form is submitted
+    if request.method == 'POST': #only enters here when the form is submitted
         available_spots = []
         latitude = request.form['latitude']
         longitude = request.form['longitude']
@@ -29,13 +30,35 @@ def getAvailableSpots():
         return "<h3>" + str(available_spots) + "</h3>"
     if request.method == 'GET':
         return '''<form method="POST">
+                      Input the latitude, longitude, and radius you would like to look for parking spots in<br>
                       Latitude: <input type="text" name="latitude"><br>
                       Longitude: <input type="text" name="longitude"><br>
                       Radius: <input type="text" name="radius"><br>
                       <input type="submit" value="Submit"><br>
                   </form>'''
 
-@app.route('/reserve')
+@app.route('/reserve', methods=['GET', 'POST'])
 def reserveSpot():
-    return 'reserving requested spot'
+    if request.method == 'POST': #only enters here when the form is submitted
+        removed = False
+        id = request.form['id']
+
+        if len(coordinates) == 0: #no parking spots are available to be reserved
+            return "<h3>" + "No parking spots can be reserved right now." + "<h3>"
+
+        for s in coordinates:
+            if s["id"] == int(id):
+                coordinates.remove(s)
+                removed = True
+                return "reserved parking spot # " + id + " has been reserved!"
+
+        if(not removed): #the car you want to reserve is not available
+            return "Parking spot #" + id + " doesn't exist or cannot be reserved right now."
+
+    if request.method == 'GET':
+        return '''<form method="POST">
+                      Input the id of the parking spot you want to reserve <br>
+                      ID: <input type="text" name="id"><br>
+                      <input type="submit" value="Submit"><br>
+                  </form>'''
 
